07d882a1a61701eb4d48b003704c6f3f2c3d38e9
diego <145928215+vpsvip7@users.noreply.github.com>
2024-11-24 17:31:28 -0300

    Actualizar python3.sh

--- python3.sh
+++ python3.sh
@@ -6,46 +6,26 @@ cor3='\033[1;35m'
 clear
 scor='\033[0m'
 echo -e "\E[44;1;37m       ELEGIR   UNA   OPCION      \E[0m"
-echo -e "  [\033[1;36m1:\033[1;31m] \033[1;37m• \033[1;32mIniciar -Reiniciar Psi \033[1;31m"
-echo -e "  [\033[1;36m2:\033[1;31m] \033[1;37m• \033[1;33mVerificar Screen \033[1;31m    "
-echo -e "   [\033[1;36m3:\033[1;31m] \033[1;37m• \033[1;33mVer Puertos Activos \033[1;31m      \E[0m"
-echo  -e "    [\033[1;36m4\033[1;31m] \033[1;37m• \033[1;33mVer Codigo Tarjet \033[1;31m  "
-echo  -e  "  [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mTestear Velocidad \033[1;31m  "
-echo  -e  "   [\033[1;36m5:\033[1;31m] \033[1;37m• \033[1;33mProbar Velocidsd \033[1;31m  "
-echo  -e " [\033[1;36m7:\033[1;31m] \033[1;37m• \033[1;33mLimpiar Ram \033[1;31m"
-echo  -e "     [\033[1;36m8:\033[1;31m] \033[1;37m• \033[1;33mBorrar Psiphon \033[1;31m "
-echo  -e "  [\033[1;36m41\033[1;31m] \033[1;37m• \033[1;33mVer Conectados \033[1;31m "
+echo -e "  [\033[1;36m1:\033[1;31m] \033[1;37m• \033[1;32m INSTALAR PYTHON3  \033[1;31m"
+echo -e "  [\033[1;36m2:\033[1;31m] \033[1;37m• \033[1;33mVERIFICAR PYTHON3 \033[1;31m    "
+echo -e "   [\033[1;36m3:\033[1;31m] \033[1;37m• \033[1;33mABRIR PYTHON3 \033[1;31m      \E[0m"
 
 #leemos del teclado sentado
 read n
 
 case $n in
         1) clear
-cd /root/psi && screen -dmS PSI ./psiphond run
-            echo -ne "\n\033[1;31mListo \033[1;33mPsiphon Iniciado o  \033[1;32mReiniciado!\033[0m"; read
+wget https://raw.githubusercontent.com/vpsvip7/VPS-AGN/main/installer/web1.sh && chmod +x web1.sh && ./web1.sh
+            echo -ne "\n\033[1;31mListo \033[1;33mPython3 ok  \033[1;32mInstalado!\033[0m"; read
            ;;
         2) clear
-        which screen
-           sleep 5 
+        netstat -tnpl
+           echo -ne "\n\033[1;31mListo \033[1;33mPuertos Activos  \033[1;32mOK!\033[0m"; read
             ;;
         3) clear
-            netstat -tnpl
+            websocket menu
              sleep 6
-           ;; 
-        4) clear
-             cd /root/psi&&cat /root/psi/server-entry.dat;echo ''
-            sleep 15
            ;;
-        5) speedtest
-        echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu!\033[0m"; read
-         ;;
-        6) apt update 
-             menu;;
-        7)     sync & sysctl -w vm.drop_caches=3 
-           menu   ;;
-         8)  rm -rf /root/psi
-             menu;;
-          9)  ./verconectados.sh
-             menu;;
+        
         *) echo "Opción Incorrecta";;
 esac
